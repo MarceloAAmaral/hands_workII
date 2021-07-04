419960eca4dd72edba66f7eba4ad74956918cb8a
MarceloAAmaral <57374333+MarceloAAmaral@users.noreply.github.com>
2021-07-04 18:49:22 -0300

    Add files via upload

---
+++ quem-somos/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <?PHP include "../html/head.php"; ?>
+    <body>
+        <div id="content-main">
+			<?PHP include "../html/menu.php"; ?>
+			<section id="quem-somos">
+				<h1>Quem somos</h1>
+				<article>Estamos correndo desde 2017. Nunca paramos, nunca desistimos. Assim seguimos em frente na busca de nossos objetivos.
+				Superando nossas metas criamos, organizamos e realizamos eventos esportivos seja 
+				corrida de rua, ciclismo ou mesmo desafios esportivos. Somos felizes no que fazemos
+				pois proporcionamos SU-PE-RA-ÇÃO. <BR>Desafie-se conosco, e descubra que você também pode, e pode ir além.				
+				</article>
+			</section> 	
+		</div>
+		<?PHP include "../html/footer.php"; ?>
+    </body>
+		<?PHP include "../inc/script.php"; ?>
+</html>
