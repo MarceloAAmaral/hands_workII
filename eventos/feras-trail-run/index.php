84b3dd62ad97db31fe626eca9b9ed5f04debe77e
MarceloAAmaral <57374333+MarceloAAmaral@users.noreply.github.com>
2021-07-04 18:52:07 -0300

    Add files via upload

---
+++ eventos/feras-trail-run/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <?PHP include "../../html/head.php"; ?>
+    <body>
+        <div id="content-main">
+			<?PHP include "../../html/menu.php"; ?>
+			<section id="desafio-dos-faraos">
+				<h1>Feras Trail Run</h1>
+				<article> Ao inscrever-se aguardar o contato de nossa equipe para as orientações de pagamento.</article>
+				<form method="POST" action="">
+					<div id="dados-atleta" class='col-1 left'>
+					<h2>Dados do atleta</h2>
+						<div class='col-2 item'>
+							<label>Nome</label>
+							<input name='nome' type='text' value=""/>
+						</div>
+						<div class='col-2 item'>
+							<label>Nascimento</label>
+							<input name='nascimento' type='date' value=""/>
+						</div>
+						<div class='col-2 item'>
+							<label>Endereço</label>
+							<input name='endereco' type='text' value=""/>
+						</div>
+						<div class='col-2 item'>
+							<label>Email</label>
+							<input name='email' type='email' value=""/>
+						</div>
+						<div class='col-4 item'>
+							<label>Bairro</label>
+							<input name='endereco' type='text' value=""/>
+						</div>
+						<div class='col-4 item'>
+							<label>N°</label>
+							<input name='endereco' type='text' value=""/>
+						</div>
+						<div class='col-4 item'>
+							<label>DDD</label>
+							<input name='ddd' type='number' value=""/>
+						</div>
+						<div class='col-4 item'>
+							<label>Telefone</label>
+							<input name='telefone' type='number' value=""/>
+						</div>
+					</div>
+					<div id="dados-inscricao" class='col-1 left'>
+					<h2>Dados da inscrição</h2>						
+						<div class='col-4 item'>
+							<label>Percurso</label>
+							<select name='percurso'>
+								<option value="">selecione</option>
+								<option value="5k">5 Km</option>
+								<option value="10k">10 Km</option>
+							</select>
+						</div>
+						<div class='col-4 item'>
+							<label>Kit</label>
+							<select name='percurso'>
+								<option value="sem-kit">Sem Kit</option>
+								<option value="com-kit">Com kit</option>
+							</select>
+						</div>
+						<div class='col-4 item'>
+							<label>Valor</label>
+							<h3>R$ 65,00</h3>
+						</div>
+					</div>
+					<div class='col-2  row-center'><input type="submit" name='enviar' value='Inscrever'/></div>
+				</form>
+			</section>
+		</div>
+		<?PHP include "../../html/footer.php"; ?>
+    </body>
+		<?PHP include "../../inc/script.php"; ?>
+</html>
---
+++ eventos/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<?PHP include "../html/head.php"; ?>
+<body>
+<div id="content-main">
+	<?PHP include "../html/menu.php"; ?>
+	<section id="eventos" >
+				<h1>Eventos</h1>
+				<div class="container col-1 flex">
+					<div class="col-4 item">
+						
+						<div class='img'>
+							<img src='<?PHP echo $url ?>img/banner_desafio_dos_faraos_chamada_1080_1080.jpg'/>
+						</div>
+						<h2> Desafio dos Faraós</h2>
+						
+						<div class='link'>
+							<a href="desafio-dos-faraos">Inscrição</a>
+							<div class='vlr right'>
+								<label>R$ 130,00</label>
+							</div>
+						</div>
+					</div>	
+					<div class="col-4 item">
+						
+						<div class='img'>
+							<img src='<?PHP echo $url ?>img/meia_maratona_ac.jpg'/>
+						</div>
+						<h2> Meia Maratona de Antônio Carlos</h2>
+						
+						<div class='link'>
+							<a href="meia-maratona-antonio-carlos">Inscrição</a>
+							<div class='vlr right'>
+								<label>R$ 95,00</label>
+							</div>
+						</div>						
+					</div>
+					<div class="col-4 item">					
+						<div class='img'>
+							<img src='<?PHP echo $url ?>img/banner_feras_trail_run_1080x1080.png'/>
+						</div>
+						<h2> Feras Trail Run</h2>
+						
+						<div class='link'>
+							<a href="feras-trail-run">Inscrição</a>
+							<div class='vlr right'>
+								<label>R$ 65,00</label>
+							</div>
+						</div>
+					</div>
+					<div class="col-4 item">						
+						<div class='img'>
+							<img src='<?PHP echo $url ?>img/banner_night_run_ingleses_1080_1080.jpg'/>
+						</div>
+						<h2> Night Run Ingleses</h2>
+							
+						<div class='link'>
+							<a href="night-run-ingleses">Inscrição</a>
+							<div class='vlr right'>
+								<label>R$ 65,00</label>
+							</div>
+						</div>
+					</div>
+				</div>				
+			</section>
+</div>	
+<?PHP include "../html/footer.php"; ?>
+</body>
+<?PHP include "../inc/script.php"; ?>
+</html>
